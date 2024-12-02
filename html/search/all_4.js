793db988a3c9d29382855f261dee4c08f91a668d
AnduDiaconu18 <denis-alexandru.diaconu@s.unibuc.ro>
2024-12-02 18:27:04 +0200

    v0.2

---
+++ html/search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cerințe_0',['Cerințe',['../md__r_e_a_d_m_e.html#autotoc_md4',1,'Cerințe'],['../md__r_e_a_d_m_e.html#autotoc_md6',1,'Cerințe'],['../md__r_e_a_d_m_e.html#autotoc_md8',1,'Cerințe']]]
+];
---
+++ html/search/all_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['proiect_0',['proiect',['../md__r_e_a_d_m_e.html#autotoc_md1',1,'Descriere proiect'],['../md__r_e_a_d_m_e.html',1,'Titlu proiect']]]
+];
